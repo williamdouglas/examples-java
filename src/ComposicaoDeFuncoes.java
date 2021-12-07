61ea96bccc2b125911e72f142084dc46198db95b
William Douglas de Oliveira Santos <williamdouglaz@hotmail.com>
2021-12-06 21:40:55 -0300

    Primeiro commit programa diferença entre paradigma funcional e imperativo

---
+++ src/ComposicaoDeFuncoes.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ComposicaoDeFuncoes {
+    public static void main(String[] args) {
+        int[] valores = { 0, 1, 2, 3, 4 };
+
+        // funcional
+        Arrays.stream(valores)
+                .filter(numero -> numero % 2 == 0)
+                .map(numero -> numero * 2)
+                .forEach(numero -> System.out.println(numero));
+
+        // imperativo
+        for (int i = 0; i < valores.length; i++) {
+            int valor = 0;
+
+            if (valores[i] % 2 == 0) {
+                valor = valores[i] * 2;
+
+                if (valor != 0) {
+                    System.out.println(valor);
+                }
+            }
+        }
+    }
+}
